1951106fa44291e9177bd6ec9030e9874eeca71d
Gavrilo Aleksic <gavrilo_aleksic_invictus@outlook.com>
2021-03-10 14:17:54 +0100

    fixed parsing comment date

--- src/utils/date.util.js
+++ src/utils/date.util.js
@@ -1,1 +1,20 @@
-export const formatDate = (date) => new Date(date).toISOString()
+const MONTHS = [
+  "Januar",
+  "February",
+  "Marth",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+export const formatDate = (date) => {
+  const parsedDate = new Date(date);
+  return `${
+    MONTHS[parsedDate.getMonth()]
+  } ${parsedDate.getDate()}, ${parsedDate.getFullYear()} at ${parsedDate.toLocaleTimeString()}`;
+};
